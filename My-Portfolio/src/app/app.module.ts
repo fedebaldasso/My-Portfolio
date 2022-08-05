7a8219359deebb49159e887b04fecd8eae675c77
Federico Baldasso <74937639+fedebaldasso@users.noreply.github.com>
2022-08-05 12:07:05 -0300

    Avance Portfolio

--- My-Portfolio/src/app/app.module.ts
+++ My-Portfolio/src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { RedesSocialesComponent } from './componentes/redes-sociales/redes-sociales.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { AboutMeComponent } from './componentes/about-me/about-me.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { FormsModule } from '@angular/forms';
+import { HysComponent } from './componentes/hys/hys.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RedesSocialesComponent,
+    BannerComponent,
+    AboutMeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HysComponent,
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // BrowserAnimationsModule,
+    FormsModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
